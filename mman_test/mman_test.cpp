c48d285d8b6b09b8a20af7a4af5bb7f07331981b
Tom Sullivan <tom@msbit.com.au>
2024-02-04 15:29:39 +1100

    chore: mman_test

---
+++ mman_test/mman_test.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace mmantest
+{
+	TEST_CLASS(mmantest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
